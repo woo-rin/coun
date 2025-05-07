f8ec35d6bb26aa03e59a7a9ea51b711635a639c7
woo <dlqk091028@gmail.com>
2025-05-07 20:10:48 +0900

    coll 추가

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 import streamlit as st
 
-st.title("this is ")
-st.header("this is header")
-st.subheader("fbwbkfk")
+col1, col2 = st.columns([2, 3])
+with col1 :
+    st.title("here is colun1 title")
+
+with col2 :
+    st.title("here is colun2 title")
+    st.checkbox("this is cheak box")
